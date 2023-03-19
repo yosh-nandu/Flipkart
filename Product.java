8c31237de3066e15de1b2a6ed952dc505060b73e
Yoshobanta garnaik <garnaikyosh@gmail.com>
2023-03-19 10:12:39 +0000

    My second commit after adding deploy.sh and some new change in Product.java

--- Product.java
+++ Product.java
@@ -2,6 +2,7 @@ class product
 {
    public static void main()
        	{
-		 system.out.println("My Flipkart world")
+		system.out.println("My Flipkart world")
+		system.out.println("I want to buy shirt")
 	}
 }
